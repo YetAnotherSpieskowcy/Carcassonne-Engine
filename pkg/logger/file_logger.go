b27558b058794e82580ade35cd9f34d898f05149
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-04-06 22:54:13 +0200

    Embedded Logger in FileLogger

--- pkg/logger/file_logger.go
+++ pkg/logger/file_logger.go
@@ -3,8 +3,8 @@ package logger
 import "os"
 
 type FileLogger struct {
-	logger Logger
-	file   *os.File
+	Logger
+	file *os.File
 }
 
 func NewFromFile(filename string) (FileLogger, error) {
@@ -16,23 +16,11 @@ func NewFromFile(filename string) (FileLogger, error) {
 func (fl *FileLogger) Open(filename string) error {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	fl.file = file
-	fl.logger = Logger{file}
+	fl.writer = file
 	return err
 }
 
 func (fl *FileLogger) Close() error {
 	err := fl.file.Close()
 	return err
-}
-
-func (fl *FileLogger) Start(deck []int, players []string) error {
-	return fl.logger.Start(deck, players)
-}
-
-func (fl *FileLogger) PlaceTile(player int, rotation int, position []int, meeple int) error {
-	return fl.logger.PlaceTile(player, rotation, position, meeple)
-}
-
-func (fl *FileLogger) End(scores []int) error {
-	return fl.logger.End(scores)
 }
