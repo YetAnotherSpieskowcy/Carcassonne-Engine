9c4c7365c28298716cf82ca43327ee6df31fce8d
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-04-07 10:46:02 +0200

    Added test for regular Logger

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -2,13 +2,14 @@ package logger
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"os"
 	"reflect"
 	"testing"
 )
 
-func TestLog(t *testing.T) {
+func TestFileLogger(t *testing.T) {
 	filename := "test_file.jsonl"
 
 	log, err := NewFromFile(filename)
@@ -109,4 +110,99 @@ func TestLog(t *testing.T) {
 	if !reflect.DeepEqual(endLine.Scores, []int{1, 2}) {
 		t.Fatal("FAILED")
 	}
+}
+func TestLogger(t *testing.T) {
+	buffer := bytes.NewBuffer(nil)
+
+	log := New(buffer)
+
+	err := log.Start([]int{1, 2, 3}, []string{"Player1", "Player2"})
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+
+	err = log.PlaceTile(0, 1, []int{1, 2}, 0)
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+
+	err = log.End([]int{1, 2})
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+
+	var startLine struct {
+		Event   string
+		Deck    []int
+		Players []string
+	}
+
+	var placeTileLine struct {
+		Event    string
+		Player   int
+		Rotation int
+		Position []int
+		Meeple   int
+	}
+
+	var endLine struct {
+		Event  string
+		Scores []int
+	}
+
+	line, err := buffer.ReadString(byte('\n'))
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+
+	err = json.Unmarshal([]byte(line), &startLine)
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+	if startLine.Event != "start" {
+		t.Fatal("FAILED")
+	}
+	if !reflect.DeepEqual(startLine.Deck, []int{1, 2, 3}) {
+		t.Fatal("FAILED")
+	}
+	if !reflect.DeepEqual(startLine.Players, []string{"Player1", "Player2"}) {
+		t.Fatal("FAILED")
+	}
+
+	line, err = buffer.ReadString(byte('\n'))
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+	err = json.Unmarshal([]byte(line), &placeTileLine)
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+	if placeTileLine.Event != "place" {
+		t.Fatal("FAILED")
+	}
+	if placeTileLine.Rotation != 1 {
+		t.Fatal("FAILED")
+	}
+	if !reflect.DeepEqual(placeTileLine.Position, []int{1, 2}) {
+		t.Fatal("FAILED")
+	}
+	if placeTileLine.Meeple != 0 {
+		t.Fatal("FAILED")
+	}
+
+	line, err = buffer.ReadString(byte('\n'))
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+	err = json.Unmarshal([]byte(line), &endLine)
+	if err != nil {
+		t.Fatal("FAILED")
+	}
+	if endLine.Event != "end" {
+		t.Fatal("FAILED")
+	}
+	if !reflect.DeepEqual(endLine.Scores, []int{1, 2}) {
+		t.Fatal("FAILED")
+	}
+
 }
