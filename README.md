eae9f8043d3b601bc9412e03747b43e4cf9d2962
Jakub Kuczys <me@jacken.men>
2024-04-07 23:29:15 +0200

    Windows

--- README.md
+++ README.md
@@ -12,11 +12,34 @@ A rule engine for the Carcassonne game.
    You can get the latest version by adding the PPA listed here and installing `golang` package after: https://go.dev/wiki/Ubuntu
 2. Install `gcc` toolchain from your distro's package repositories (for example, `build-essential` package on Ubuntu).
 
+### Windows
+
+We provide you with 2 ways of installing pre-requirements - manual installation or automated installation with Chocolatey:
+
+#### Manually installing pre-requirements
+
+1. Install Go for Windows x86-64: https://go.dev/dl/
+2. Install MinGW-w64: https://github.com/niXman/mingw-builds-binaries
+
+#### Installing pre-requirements with Chocolatey
+
+*These instructions assume that you already have Chocolatey installed. If not, you can install it by following its install documentation: https://chocolatey.org/install*
+
+Run PowerShell as Administrator and execute the following command:
+```console
+choco install golang mingw
+```
+
 ## Building sources
 
 You can either use the default make target:
+- Linux
 ```console
 make
+```
+- Windows
+```console
+./make.ps1
 ```
 or run the build command manually:
 ```console
@@ -28,8 +51,13 @@ This will build all Go source files.
 ## Running the test suite
 
 You can either use the `test` make target:
+- Linux
 ```console
 make test
+```
+- Windows
+```console
+./make.ps1 test
 ```
 or run the test command manually:
 ```console
@@ -37,8 +65,13 @@ go test -race "-coverprofile=coverage.txt" "./..."
 ```
 
 To show coverage, you can either use the `show-coverage` make target:
+- Linux
 ```console
 make show-coverage
+```
+- Windows
+```console
+./make.ps1 show-coverage
 ```
 or run the cover tool command manually:
 ```console
---
+++ make.ps1
@@ -0,0 +1,78 @@
+<#
+.Synopsis
+Makefile script in PowerShell that contains commands useful during development of Carcassonne-Engine.
+
+.Description
+Available commands:
+   build             Build all Go source files.
+   test              Run the test suite.
+   open-coverage     Show coverage in the browser after running the test suite.
+
+.Parameter Command
+Command to execute. See Cmdlet's description for more information.
+
+#>
+
+# I'm too dumb for PowerShell, so $script:availableCommands needs to be defined in 2 places // Jack
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [ArgumentCompleter({
+        param (
+            $commandName,
+            $parameterName,
+            $wordToComplete,
+            $commandAst,
+            $fakeBoundParameters
+        )
+        $script:availableCommands = @("build", "test", "open-coverage")
+        return $script:availableCommands | Where-Object { $_ -like "$wordToComplete*" }
+    })]
+    [String]
+    $command,
+    [switch]
+    $help = $false
+)
+
+function build() {
+    Write-Host "Building the project..."
+    & go build "./..."
+    Exit $LASTEXITCODE
+}
+
+function test() {
+    Write-Host "Running the test suite..."
+    & go test -race "-coverprofile=coverage.txt" "./..."
+    Exit $LASTEXITCODE
+}
+
+function open-coverage() {
+    & go tool cover "-html=coverage.txt"
+    Write-Host "Coverage opened in the default browser."
+}
+
+$script:availableCommands = @("build", "test", "open-coverage")
+
+if (!$command) {
+    $command = "build"
+}
+
+if ($help) {
+    Get-Help $MyInvocation.InvocationName
+    exit
+}
+
+switch ($command) {
+    {$script:availableCommands -contains $_} {
+        & $command
+        break
+    }
+    default {
+        Write-Host (
+            """$command"" is not a valid command.",
+            "To see available commands, type: ""$($MyInvocation.InvocationName) -help"""
+        )
+        break
+    }
+}
