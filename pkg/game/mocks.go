9f23f424f581a4d4107e00ccf642410665cb1e4c
Jakub Kuczys <me@jacken.men>
2024-04-08 21:35:31 +0200

    Add mocks for Tile.Rotate() and two tile templates

--- pkg/game/mocks.go
+++ pkg/game/mocks.go
@@ -4,18 +4,30 @@ import "github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/stack"
 
 // TODO: replace all of these with an import once full tile representation is defined
 
-type Tile struct {}
 type Side int64
+type Tile struct {}
+
+func (tile Tile) Rotate(rotations uint) Tile {
+	return Tile{}
+}
 
 const (
 	None Side = iota
-	Top
+	Bottom
 )
 
 var (
 	StartingTile = PlacedTile{}
 	BaseTileSet  = []Tile{}
 )
+
+func SingleCityEdgeNoRoads() Tile {
+	return Tile{}
+}
+
+func FourCityEdgesConnectedShield() Tile {
+	return Tile{}
+}
 
 // TODO: replace this with an import once logger is defined
 type Logger struct {}
