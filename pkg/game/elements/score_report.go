0b552d77a01bdc3d90b29ff8adcb2ac6c8818b48
Jakub Kuczys <me@jacken.men>
2024-04-15 13:29:11 +0200

    Add missing bracket

--- pkg/game/elements/score_report.go
+++ pkg/game/elements/score_report.go
@@ -1,7 +1,7 @@
 package elements
 
 type ScoreReport struct {
-	// ReceivedPoints[playerID (uint8) = player's received points
+	// ReceivedPoints[playerID (uint8)] = player's received points
 	ReceivedPoints map[uint8]uint32
 	// ReturnedMeeples[playerID (uint8)][meeple type (MeepleType)] = number of returned meeples
 	// for reference, see also: player.meepleCounts
