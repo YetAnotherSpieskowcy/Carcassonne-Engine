80583432979fa17018e2f9549e7aff090ec28a64
BognaLew <100587236+BognaLew@users.noreply.github.com>
2024-04-16 16:21:54 +0200

    added checking if city is closed

--- pkg/game/city/city.go
+++ pkg/game/city/city.go
@@ -3,6 +3,7 @@ package city
 import (
 	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/elements"
 	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/feature"
+	sideMod "github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/side"
 )
 
 // Represents cities on board
@@ -24,8 +25,40 @@ func (city City) GetCompleted() bool {
 	return city.completed
 }
 
-func (city City) CheckCompleted() bool {
-	// TODO: check if city is closed
+func (city *City) CheckCompleted() bool {
+	city.completed = true
+	for key, features := range city.cities {
+		for _, feature := range features {
+			for _, side := range feature.Sides {
+				switch side {
+				case sideMod.Top:
+					_, ok := city.GetFeaturesFromTile(elements.NewPosition(key.X(), key.Y()+1))
+					if !ok {
+						city.completed = false
+						continue
+					}
+				case sideMod.Bottom:
+					_, ok := city.GetFeaturesFromTile(elements.NewPosition(key.X(), key.Y()-1))
+					if !ok {
+						city.completed = false
+						continue
+					}
+				case sideMod.Right:
+					_, ok := city.GetFeaturesFromTile(elements.NewPosition(key.X()+1, key.Y()))
+					if !ok {
+						city.completed = false
+						continue
+					}
+				case sideMod.Left:
+					_, ok := city.GetFeaturesFromTile(elements.NewPosition(key.X()-1, key.Y()))
+					if !ok {
+						city.completed = false
+						continue
+					}
+				}
+			}
+		}
+	}
 	return city.completed
 }
 
@@ -34,14 +67,14 @@ func (city City) GetFeaturesFromTile(pos elements.Position) ([]feature.Feature, 
 	return cities, ok
 }
 
-func (city City) AddTile(pos elements.Position, cityFeatures []feature.Feature) {
+func (city *City) AddTile(pos elements.Position, cityFeatures []feature.Feature) {
 	city.cities[pos] = cityFeatures
 	city.CheckCompleted()
 }
 
 // Merges two cities when they are connetced.
 // Other city must be deleted after to avoid problems
-func (city City) JoinCities(other City) {
+func (city *City) JoinCities(other City) {
 	for key, otherFeature := range other.cities {
 		feature, ok := city.GetFeaturesFromTile(key)
 		if ok {
