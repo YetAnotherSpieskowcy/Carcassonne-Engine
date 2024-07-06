3dcc6740eadae7d08a03902ac61b04cd0d0178f0
Jakub Kuczys <me@jacken.men>
2024-07-06 16:05:21 +0200

    Improve soundness of the setuptools configuration

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import os
 import subprocess
 
 import setuptools
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.build import build
 
 PY_PACKAGE_DIR = "python_bindings/carcassonne_engine"
+GO_MOD = "go.mod"
+GO_PKG_DIR = "pkg"
 GO_NAMESPACE = "github.com/YetAnotherSpieskowcy/Carcassonne-Engine"
-GO_BASE_PACKAGE = f"{GO_NAMESPACE}/pkg"
+GO_BASE_PACKAGE = f"{GO_NAMESPACE}/{GO_PKG_DIR}"
 GO_MAIN_PACKAGE_NAME = "engine"
 GO_EXCLUDED_PACKAGES = (
     # avoid listing main package twice
@@ -16,45 +18,68 @@ GO_EXCLUDED_PACKAGES = (
     "stack",
 )
 GO_MAIN_PACKAGE = f"{GO_BASE_PACKAGE}/{GO_MAIN_PACKAGE_NAME}"
-GO_PACKAGES = tuple(
-    f"{GO_NAMESPACE}/{pkg}"
-    for pkg, _, _ in os.walk("pkg")
-    if pkg != "pkg" and pkg[4:] not in GO_EXCLUDED_PACKAGES
-)
 
 
 class BinaryDistribution(setuptools.Distribution):
-    def has_ext_modules(_):
+    def has_ext_modules(self):
         return True
 
 
-def generate_and_build_extension_files():
-    subprocess.check_call(
-        (
-            "gopy",
-            "build",
-            "-output",
-            PY_PACKAGE_DIR,
-            GO_MAIN_PACKAGE,
-            *GO_PACKAGES,
-        )
-    )
-    with open(f"{PY_PACKAGE_DIR}/__init__.py", "w", encoding="utf-8") as fp:
-        fp.write("from .engine import *\n")
+class BuildGoCommand(setuptools.Command):
+    command_name = "build_go"
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def get_source_files(self) -> list[str]:
+        source_files = [GO_MOD]
+        for dirname, _, files in os.walk(GO_PKG_DIR):
+            for filename in files:
+                source_files.append(os.path.join(dirname, filename))
+        return source_files
+
+    def run(self) -> None:
+        go_packages = [
+            f"{GO_NAMESPACE}/{pkg}"
+            for pkg, _, _ in os.walk(GO_PKG_DIR)
+            if pkg[4:] not in GO_EXCLUDED_PACKAGES
+        ][1:]
+
+        init_filepath = os.path.join(PY_PACKAGE_DIR, "__init__.py")
+        with open(init_filepath, "rb") as fp:
+            init_contents = fp.read()
+        try:
+            subprocess.check_call(
+                (
+                    "gopy",
+                    "build",
+                    # though this would be nicer to work with on Python side,
+                    # it is buggy and converts only some occurrences to camel_case
+                    # breaking the whole library in the process
+                    # "-rename=true",
+                    "-output",
+                    PY_PACKAGE_DIR,
+                    GO_MAIN_PACKAGE,
+                    *go_packages,
+                )
+            )
+        finally:
+            with open(init_filepath, "wb") as fp:
+                fp.write(init_contents)
+            try:
+                os.remove(os.path.join(PY_PACKAGE_DIR, "build.py"))
+            except FileNotFoundError:
+                pass
 
 
-# TODO(Jackenmen): figure out correct place to generate / build extension files
-class build_ext(_build_ext):
-    def build_extensions(self) -> None:
-        generate_and_build_extension_files()
-        super().build_extensions()
-
-
-# this is just a (slightly slower) workaround
-generate_and_build_extension_files()
+class CustomBuild(build):
+    sub_commands = [("build_go", None)] + build.sub_commands
 
 
 setuptools.setup(
-    include_package_data=True,
     distclass=BinaryDistribution,
+    cmdclass={"build": CustomBuild, "build_go": BuildGoCommand},
 )
