e410101ffef54356750da97bade6cc4190ba94d3
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-07-11 11:12:42 +0200

    Adding positions test

--- pkg/game/position/position_test.go
+++ pkg/game/position/position_test.go
@@ -5,6 +5,32 @@ import (
 	"testing"
 )
 
+func TestAddPositions(t *testing.T) {
+	pos1 := NewPosition(3, 5)
+	pos2 := NewPosition(1, 2)
+	added := pos1.Add(pos2)
+
+	if !(added.X() == 4 && added.Y() == 7) {
+		t.Fatalf("expected (%#v,%#v), got (%#v,%#v) instead", 4, 7, added.X(), added.Y())
+	}
+
+	pos1 = NewPosition(-3, 5)
+	pos2 = NewPosition(1, -4)
+	added = pos1.Add(pos2)
+
+	if !(added.X() == -2 && added.Y() == 1) {
+		t.Fatalf("expected (%#v,%#v), got (%#v,%#v) instead", -2, 1, added.X(), added.Y())
+	}
+
+	pos1 = NewPosition(0, 5)
+	pos2 = NewPosition(-1, 0)
+	added = pos1.Add(pos2)
+
+	if !(added.X() == -1 && added.Y() == 5) {
+		t.Fatalf("expected (%#v,%#v), got (%#v,%#v) instead", -1, 5, added.X(), added.Y())
+	}
+}
+
 func TestPositionMarshalTextWithPositiveCoords(t *testing.T) {
 	pos := NewPosition(1, 3)
 	expected := []byte("1,3")
