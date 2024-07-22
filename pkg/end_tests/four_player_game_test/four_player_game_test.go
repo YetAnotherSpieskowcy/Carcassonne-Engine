69d8c9bf71fa2acac7ae6bd111aa4808d53e7ef3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-07-22 19:18:35 +0200

    2 game tests + automatic test job

---
+++ pkg/end_tests/four_player_game_test/four_player_game_test.go
@@ -0,0 +1,539 @@
+//go:build test
+
+package four_player_game_test
+
+import (
+	"testing"
+
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/deck"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/end_tests"
+	gameMod "github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/elements"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/position"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/stack"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/feature"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/side"
+)
+
+/*
+ diagonal edges represent cities, dots fields, straight lines roads. The big vertical line on the left is to prevent comment formating
+ Player meeples will be represented as !@#$ signs ( you know, writing number but with shift!) 1->!, 2->@ and so on
+
+ Final board: (each tile is represented by 5x5 ascii signs, at the center is the turn number in hex :/)
+
+|				  0	   1    2    3
+|
+|
+|               .| |................
+|				.\ /................
+|1				..2....4----9---[A].
+|				./ \...|.../ \......
+|				.| |...|...| |......
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........|.../ \.
+|	       ..|.........|...| |.
+|	       ..|.........|...| |.
+|	       ..|.........|...\ /
+|-1		   ..B----6----5....7..
+|	       ..|............./ \.
+|	       ..|.............| |.
+|				.....
+|				.....
+|-2				--C--
+|				.....
+|				.....
+*/
+
+func Test4PlayerFullGame(t *testing.T) {
+	// create game
+	minitileSet := end_tests.MiniTileSetRoadsAndFields()
+	deckStack := stack.NewSeeded(minitileSet.Tiles, 94058654839) // random seed :P
+	deck := deck.Deck{Stack: &deckStack, StartingTile: minitileSet.StartingTile}
+	game, err := gameMod.NewFromDeck(deck, nil, 4)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	// for i, _ := range deck.GetTiles() {
+	// 	fmt.Printf("%v:\n", i+1)
+	// 	var tile, _ = deck.Next()
+	// 	fmt.Printf("%v\n", tile)
+	// }
+	checkFirstTurn(game, t)    // T Cross road
+	checkSecondTurn(game, t)   // Two city edges not connected
+	checkThirdTurn(game, t)    // Two city edges not connected
+	checkFourthTurn(game, t)   // Road turn
+	checkFifthTurn(game, t)    // Road Turn
+	checkSixthTurn(game, t)    // Straight Road
+	checkSeventhTurn(game, t)  // Two city edges not connected
+	checkEightthTurn(game, t)  // Road turn
+	checkNinethTurn(game, t)   // Straight road with city edge
+	checkTenthTurn(game, t)    // Monastery with road
+	checkEleventhTurn(game, t) // T cross road
+	checkTwelvethTurn(game, t) // Straight road
+	checkFinalResult(game, t)
+
+}
+
+/*
+// player1 places T Cross road with meeple on a bottom road
+
+|				  0	   1    2    3
+|
+|
+|
+|
+|1
+|
+|
+|		        .| |...|..
+|		        .\ /...|..
+|0		        --0----1..
+|	            .......!..
+|	            .......|..
+|
+|
+|-1
+|
+|
+|
+|
+|-2
+|
+|
+*/
+func checkFirstTurn(game *gameMod.Game, t *testing.T) {
+
+	pos := position.New(1, 0)
+	end_tests.MakeTurn(game, t, pos, 1, elements.NormalMeeple, side.Bottom, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.Road, true, 1)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 0, 0, 0}, []uint8{6, 7, 7, 7}, 1)
+}
+
+/*
+// player2 places Two city edges not connected, and a meeple on a closing city
+
+|				  0	   1    2    3
+|
+|
+|               .| |.
+|				.\ /.
+|1				..2..
+|				./ \.
+|				.| |.
+|		        .| |...|..
+|		        .\ /...|..
+|0		        --0----1..
+|	            .......!..
+|	            .......|..
+|
+|
+|-1
+|
+|
+|
+|
+|-2
+|
+|
+*/
+func checkSecondTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(0, 1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Bottom, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.City, false, 2)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 0, 0}, []uint8{6, 7, 7, 7}, 2)
+}
+
+/*
+// Player3 places Two city edges not connected and a meeple on bottom city
+
+|				  0	   1    2    3
+|
+|
+|               .| |.
+|				.\ /.
+|1				..2..
+|				./ \.
+|				.| |.
+|		        .| |...|...| |.
+|		        .\ /...|...\ /.
+|0		        --0----1....3..
+|	            .......!.../ \.
+|	            .......|...|#|.
+|
+|
+|-1
+|
+|
+|
+|
+|-2
+|
+|
+*/
+func checkThirdTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(2, 0)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Bottom, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.City, true, 3)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 0, 0}, []uint8{6, 7, 6, 7}, 3)
+
+}
+
+/*
+// Player4 places Road turn with a meeple on a road
+
+|				  0	   1    2    3
+|
+|
+|               .| |......
+|				.\ /......
+|1				..2....4-$
+|				./ \...|..
+|				.| |...|..
+|		        .| |...|...| |.
+|		        .\ /...|...\ /.
+|0		        --0----1....3..
+|	            .......!.../ \.
+|	            .......|...|#|.
+|
+|
+|-1
+|
+|
+|
+|
+|-2
+|
+|
+*/
+func checkFourthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, 1)
+	end_tests.MakeTurn(game, t, pos, 3, elements.NormalMeeple, side.Right, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Right, feature.Road, true, 4)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 0, 0}, []uint8{6, 7, 6, 6}, 4)
+}
+
+/*
+// Player1 places Road turn with a farmer on the right bototn
+
+|				  0	   1    2    3
+|
+|
+|               .| |......
+|				.\ /......
+|1				..2....4-$
+|				./ \...|..
+|				.| |...|..
+|		        .| |...|...| |.
+|		        .\ /...|...\ /.
+|0		        --0----1....3..
+|	            .......!.../ \.
+|	            .......|...|#|.
+|	                 ..|..
+|	                 ..|..
+|-1		             --5..
+|	                 ...!.
+|	                 .....
+|
+|
+|-2
+|
+|
+*/
+func checkFifthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, -1)
+	end_tests.MakeTurn(game, t, pos, 1, elements.NormalMeeple, side.RightBottomEdge, feature.Field)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.BottomRightEdge, feature.Field, true, 5)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 0, 0}, []uint8{5, 7, 6, 6}, 5)
+}
+
+/*
+// Player2 places Straight Road with a meeple on a top field
+
+|				  0	   1    2    3
+|
+|
+|               .| |......
+|				.\ /......
+|1				..2....4-$
+|				./ \...|..
+|				.| |...|..
+|		        .| |...|...| |.
+|		        .\ /...|...\ /.
+|0		        --0----1....3..
+|	            .......!.../ \.
+|	            .......|...|#|.
+|	            ..@....|..
+|	            .......|..
+|-1		        --6----5..
+|	            ........!.
+|	            ..........
+|
+|
+|-2
+|
+|
+*/
+func checkSixthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(0, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Top, feature.Field)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Top, feature.Field, true, 6)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 0, 0}, []uint8{5, 6, 6, 6}, 6)
+}
+
+/*
+// Player3 places Two city edges not connected, finishing own city, and placing a meeple on a new one
+
+|				  0	   1    2    3
+|
+|
+|               .| |......
+|				.\ /......
+|1				..2....4-$
+|				./ \...|..
+|				.| |...|..
+|		        .| |...|...| |.
+|		        .\ /...|...\ /.
+|0		        --0----1....3..
+|	            .......!.../ \.
+|	            .......|...| |.
+|	            ..@....|...| |.
+|	            .......|...\ /
+|-1		        --6----5....7..
+|	            ........!../ \.
+|	            ...........|#|.
+|
+|
+|-2
+|
+|
+*/
+
+func checkSeventhTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(2, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Bottom, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(2, 0), side.Bottom, feature.City, false, 7) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.City, true, 7)                 // new meeple
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 4, 0}, []uint8{5, 6, 6, 6}, 7)
+}
+
+/*
+// Player4 places Road turn and a meeple on a road
+
+|				  0	   1    2    3
+|
+|
+|               .| |......
+|				.\ /......
+|1				..2....4-$
+|				./ \...|..
+|				.| |...|..
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........!.../ \.
+|	       ..$.........|...| |.
+|	            ..@....|...| |.
+|	            .......|...\ /
+|-1		        --6----5....7..
+|	            ........!../ \.
+|	            ...........|#|.
+|
+|
+|-2
+|
+|
+*/
+func checkEightthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(-1, 0)
+	end_tests.MakeTurn(game, t, pos, 3, elements.NormalMeeple, side.Bottom, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.Road, true, 8)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 4, 0}, []uint8{5, 6, 6, 5}, 8)
+}
+
+/*
+// Player1 places Straight road with city edge and places a meeple on the upper field.
+No one scores for the finished city
+
+|				  0	   1    2    3
+|
+|
+|               .| |........!..
+|				.\ /...........
+|1				..2....4-$--9--
+|				./ \...|.../ \.
+|				.| |...|...| |.
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........!.../ \.
+|	       ..$.........|...| |.
+|	            ..@....|...| |.
+|	            .......|...\ /
+|-1		        --6----5....7..
+|	            ........!../ \.
+|	            ...........|#|.
+|
+|
+|-2
+|
+|
+*/
+func checkNinethTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(2, 1)
+	end_tests.MakeTurn(game, t, pos, 2, elements.NormalMeeple, side.Top, feature.Field)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Top, feature.Field, true, 9)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 4, 0}, []uint8{4, 6, 6, 5}, 9)
+}
+
+/*
+// Player2 places Monastery with road, with a meeple on a monastery.
+Player4 scores 3 points for finished road
+
+|				  0	   1    2    3
+|
+|
+|               .| |........!.......
+|				.\ /............[ ].
+|1				..2....4----9---[A].
+|				./ \...|.../ \..[@].
+|				.| |...|...| |......
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........!.../ \.
+|	       ..$.........|...| |.
+|	            ..@....|...| |.
+|	            .......|...\ /
+|-1		        --6----5....7..
+|	            ........!../ \.
+|	            ...........|#|.
+|
+|
+|-2
+|
+|
+*/
+func checkTenthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(3, 1)
+	end_tests.MakeTurn(game, t, pos, 1, elements.NormalMeeple, side.NoSide, feature.Monastery)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(1, 1), side.Right, feature.Road, false, 10) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.NoSide, feature.Field, true, 10)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{0, 4, 4, 3}, []uint8{4, 5, 6, 6}, 10)
+}
+
+/*
+// Player3 places T cross road, with a meeple on a bottom road
+player1 and player4 score 4 points for their roads
+
+|				  0	   1    2    3
+|
+|
+|               .| |........!.......
+|				.\ /............[ ].
+|1				..2....4----9---[A].
+|				./ \...|.../ \..[@].
+|				.| |...|...| |......
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........|.../ \.
+|	       ..|.........|...| |.
+|	       ..|....@....|...| |.
+|	       ..|.........|...\ /
+|-1		   ..B----6----5....7..
+|	       ..|..........!../ \.
+|	       ..#.............|#|.
+|
+|
+|-2
+|
+|
+*/
+func checkEleventhTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(3, 1)
+	end_tests.MakeTurn(game, t, pos, 3, elements.NormalMeeple, side.NoSide, feature.Monastery)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(-1, 0), side.Bottom, feature.Road, false, 11) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, position.New(0, 1), side.Bottom, feature.Road, false, 11)  // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.City, true, 11)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 4, 4, 7}, []uint8{5, 5, 5, 7}, 11)
+}
+
+/*
+// Player4 places Straight road with a meeple on a road
+
+|				  0	   1    2    3
+|
+|
+|               .| |........!.......
+|				.\ /............[ ].
+|1				..2....4----9---[A].
+|				./ \...|.../ \..[@].
+|				.| |...|...| |......
+|		   ......| |...|...| |.
+|		   ......\ /...|...\ /.
+|0		   ..8----0----1....3..
+|	       ..|.........|.../ \.
+|	       ..|.........|...| |.
+|	       ..|....@....|...| |.
+|	       ..|.........|...\ /
+|-1		   ..B----6----5....7..
+|	       ..|..........!../ \.
+|	       ..#.............|#|.
+|				.....
+|				.....
+|-2				--C-$
+|				.....
+|				.....
+*/
+
+func checkTwelvethTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(0, -2)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Right, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Right, feature.Road, true, 12)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 4, 4, 7}, []uint8{5, 5, 5, 6}, 12)
+}
+
+/*
+player 1 scores additional 15 points:
+  - 9 points for upper farmer
+  - 6 points for lower farmer
+
+player 2 scores additional 2 points:
+  - 2 points for a monastery
+  - 0 for farmer in the center
+
+player 3 scores additional 2 points:
+  - 1 point for an unfished city
+  - 1 point for an unfished road
+
+player 4 scores additional 1 point:
+  - 1 point for an unfished road
+*/
+func checkFinalResult(game *gameMod.Game, t *testing.T) {
+	var scores, err = game.Finalize()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	var expectedScores = []uint32{4 + 15, 4 + 2, 4 + 2, 7 + 1}
+
+	for i := range 4 {
+		if scores.ReceivedPoints[elements.ID(i)] != expectedScores[i] {
+			t.Fatalf("Player %d final score incorrect. Expected %d, got: %d", i+1, expectedScores[i], scores.ReceivedPoints[1])
+		}
+	}
+}
---
+++ pkg/end_tests/two_player_game_test/two_player_game_test.go
@@ -0,0 +1,359 @@
+//go:build test
+
+package two_player_game_test
+
+import (
+	"testing"
+
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/deck"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/end_tests"
+	gameMod "github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/elements"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/position"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/stack"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/feature"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/side"
+)
+
+/*
+ diagonal edges represent cities, dots fields, straight lines roads. The big vertical line on the left is to prevent comment formating
+ Final board: (each tile is represented by 3x3 ascii signs, at the center is the turn number in hex :/)
+| 	.|........|.
+| 	.9--1--2..8.
+| 	.|./ \ |..|.
+| 	.| \ / |..|....
+| 	.4--0--3..B--C-
+| 	.|.............
+|	.|.   .........
+| 	.5.   >6<>7<>A<
+| 	...   .........
+*/
+
+func Test2PlayerFullGame(t *testing.T) {
+	// create game
+	minitileSet := end_tests.MiniTileSetRoadsAndFields()
+	deckStack := stack.NewSeeded(minitileSet.Tiles, 47328235) // random seed :P
+	deck := deck.Deck{Stack: &deckStack, StartingTile: minitileSet.StartingTile}
+	game, err := gameMod.NewFromDeck(deck, nil, 2)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	/*
+		for i, _ := range deck.GetTiles() {
+			fmt.Printf("%v:\n", i+1)
+			var tile, _ = deck.Next()
+			fmt.Printf("%v\n", tile)
+		}
+	*/
+
+	checkFirstTurn(game, t)    // straight road with city edge
+	checkSecondTurn(game, t)   // road turn
+	checkThirdTurn(game, t)    // road turn
+	checkFourthTurn(game, t)   // T cross road
+	checkFifthTurn(game, t)    // monastery with single road
+	checkSixthTurn(game, t)    // Two city edges not connected
+	checkSeventhTurn(game, t)  // Two city edges not connected
+	checkEightthTurn(game, t)  // straight road
+	checkNinethTurn(game, t)   // T cross road
+	checkTenthTurn(game, t)    // Two city edges not connected
+	checkEleventhTurn(game, t) // road turn
+	checkTwelvethTurn(game, t) // straight road
+	checkFinalResult(game, t)
+
+}
+
+// straight road with city edge
+// player 1 places meeple on city, and closes it
+/*
+|       0
+|
+| 	   ...
+|1 	   -1-
+| 	   / \
+| 	   \ /
+|0 	   -0-
+| 	   ...
+*/
+func checkFirstTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(0, 1)
+	end_tests.MakeTurn(game, t, pos, 2, elements.NormalMeeple, side.Bottom, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.City, false, 1) // removed meeple
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 0}, []uint8{7, 7}, 1)
+}
+
+// road turn
+// player 2 places meeple (M) on a road
+/*
+|       0  1
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	   \ /
+|0 	   -0-
+| 	   ...
+*/
+func checkSecondTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, 1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Left, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Left, feature.Road, true, 2)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 0}, []uint8{7, 6}, 2)
+}
+
+// road turn
+// player 1 places meeple (m) on a field
+/*
+|       0  1
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	   \ /m|.
+|0 	   -0--3.
+| 	   ......
+*/
+func checkThirdTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, 0)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NoneMeeple, side.NoSide, feature.NoneType)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.TopLeftEdge, feature.Field, true, 3)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 0}, []uint8{6, 6}, 3)
+}
+
+// T cross road
+// player2 places meeple on road going down
+/*
+|   -1  0  1
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.M.......
+*/
+func checkFourthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(-1, 0)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Bottom, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.Road, true, 4)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 0}, []uint8{6, 5}, 4)
+}
+
+// monastery with single road
+// player1 places meeple on a monastery
+// road from 4 to 5 is finished, so player2 scores 2 points
+/*
+|   -1  0  1
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.
+|-1 .5.
+| 	...
+*/
+func checkFifthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(-1, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.NoSide, feature.Monastery)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.NoSide, feature.Monastery, true, 5)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 2}, []uint8{5, 6}, 5)
+
+}
+
+// Two city edges not connected
+// player 2 places meeple(M) on the right city
+/*
+|   -1  0  1
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.   ...
+|-1 .5.   >6<M
+| 	...   ...
+*/
+func checkSixthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Right, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Right, feature.City, true, 6)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 2}, []uint8{5, 5}, 6)
+}
+
+// Two city edges not connected
+// player 1 places meeple on the right city
+// playey 2 scores points for finished city
+
+/*
+|   -1  0  1  2
+|
+| 	   ......
+|1 	   -1-M2.
+| 	   / \ |.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.   ......
+|-1	.5.   >6<>7<m
+| 	...   ......
+*/
+func checkSeventhTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(2, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Right, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(1, -1), side.Right, feature.City, false, 7) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Right, feature.City, true, 7)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 6}, []uint8{4, 6}, 7)
+}
+
+// straight road
+// player 2 places meeple on a bottom road
+
+/*
+|   -1  0  1  2  3
+|
+| 	   .......|.
+|1 	   -1-M2..8.
+| 	   / \ |..M.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.   ......
+|-1 .5.   >6<>7<m
+| 	...   ......
+*/
+func checkEightthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(1, 1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Bottom, feature.Road)
+
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Bottom, feature.Road, true, 8)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 6}, []uint8{4, 5}, 8)
+}
+
+// T cross road
+// road is finished. Player 2 scores 6 points for a road
+// player 1 places meeple (m) on a field
+
+/*
+|   -1  0  1  2  3
+|
+| 	.|m.......|.
+|1 	.9--1--2..8.
+| 	.|./ \ |..M.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.   ......
+|-1	.5.   >6<>7<m
+| 	...   ......
+*/
+func checkNinethTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(-1, 1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.TopRightEdge, feature.Field)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(1, 1), side.Left, feature.Road, false, 9) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.TopRightEdge, feature.Field, true, 9)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{4, 12}, []uint8{3, 6}, 9)
+}
+
+// Two city edges not connected
+// player 2 places meeple (M) on the right city
+// player 1 scores points for city
+
+/*
+|   -1  0  1  2  3
+|
+| 	.|m.......|.
+|1 	.9--1--2..8.
+| 	.|./ \ |..M.
+| 	.| \ /m|.
+|0 	.4--0--3.
+| 	.|.......
+|	.|.   .........
+|-1	.5.   >6<>7<>A<M
+| 	...   .........
+*/
+func checkTenthTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(3, -1)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NormalMeeple, side.Right, feature.City)
+
+	end_tests.VerifyMeepleExistence(t, game, position.New(2, 1), side.Right, feature.City, false, 10) // removed meeple
+	end_tests.VerifyMeepleExistence(t, game, pos, side.Right, feature.City, true, 10)
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{8, 12}, []uint8{4, 5}, 10)
+}
+
+// road turn
+
+/*
+|   -1  0  1  2  3
+|
+| 	.|m.......|.
+|1 	.9--1--2..8.
+| 	.|./ \ |..M.
+| 	.| \ /m|..|.
+|0 	.4--0--3..B-
+| 	.|..........
+|	.|.   .........
+|-1	.5.   >6<>7<>A<M
+| 	...   .........
+*/
+func checkEleventhTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(2, 0)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NoneMeeple, side.NoSide, feature.NoneType)
+
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{8, 12}, []uint8{4, 5}, 11)
+}
+
+// straight road
+/*
+|   -1  0  1  2  3
+|
+| 	.|m.......|.
+|1 	.9--1--2..8.
+| 	.|./ \ |..M.
+| 	.| \ /m|..|....
+|0 	.4--0--3..B--C-
+| 	.|.............
+|	.|.   .........
+|-1	.5.   >6<>7<>A<M
+| 	...   .........
+*/
+func checkTwelvethTurn(game *gameMod.Game, t *testing.T) {
+	pos := position.New(3, 0)
+	end_tests.MakeTurn(game, t, pos, 0, elements.NoneMeeple, side.NoSide, feature.NoneType)
+
+	end_tests.CheckMeeplesAndScore(game, t, []uint32{8, 12}, []uint8{4, 5}, 12)
+}
+
+/* player 1 scores additional 11 points:
+	- 2 points for monastery
+	- 3 points for farmer in the center
+	- 6 points for farmer outside
+   player 2 scores additional 4 points:
+	- 3 points for a road
+	- 1 point for unfishied city
+*/
+
+func checkFinalResult(game *gameMod.Game, t *testing.T) {
+	var scores, err = game.Finalize()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if scores.ReceivedPoints[0] != 19 {
+		t.Fatalf("Player 1 final score incorrect. Expected 19, got: %d", scores.ReceivedPoints[1])
+	}
+
+	if scores.ReceivedPoints[1] != 16 {
+		t.Fatalf("Player 2 final score incorrect. Expected 16, got: %d", scores.ReceivedPoints[1])
+	}
+}
