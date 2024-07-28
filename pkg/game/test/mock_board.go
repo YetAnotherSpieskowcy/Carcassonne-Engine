92f95f9e3ad01d466dfe68b90672af2afc463a01
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-07-28 16:26:52 +0200

    linter fix 3

--- pkg/game/test/mock_board.go
+++ pkg/game/test/mock_board.go
@@ -58,5 +58,5 @@ func (board *BoardMock) PlaceTile(
 	return board.PlaceTileFunc(tile)
 }
 
-func (board *BoardMock) RemoveMeeple(pos position.Position, s side.Side, featureType feature.Type) {
+func (board *BoardMock) RemoveMeeple(_ position.Position, _ side.Side, _ feature.Type) {
 }
