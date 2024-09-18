23697c8947e45918c0aaf2fc33cf63f2879407b0
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-09-18 16:47:48 +0200

    coverage

---
+++ pkg/game/game_test_utils_functions_test.go
@@ -0,0 +1,60 @@
+package game
+
+import (
+	"testing"
+
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/deck"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/elements"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/game/position"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/stack"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/feature"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tiles/side"
+	"github.com/YetAnotherSpieskowcy/Carcassonne-Engine/pkg/tilesets"
+)
+
+// Tests mostly made for coverage.....
+
+func TestMakeTurnValidCheck(t *testing.T) {
+	// create game
+	minitileSet := tilesets.OrderedMiniTileSet2()
+	deckStack := stack.NewOrdered(minitileSet.Tiles)
+	deck := deck.Deck{Stack: &deckStack, StartingTile: minitileSet.StartingTile}
+	game, err := NewFromDeck(deck, nil, 4)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	MakeTurnValidCheck(game, t, position.New(0, 1), MeepleParams{MeepleType: elements.NormalMeeple, FeatureSide: side.Bottom, FeatureType: feature.Road}, false, 1) // do any wrong move, and catch it
+	MakeTurnValidCheck(game, t, position.New(1, 0), MeepleParams{MeepleType: elements.NormalMeeple, FeatureSide: side.Bottom, FeatureType: feature.Road}, true, 1)  // do any correct move
+
+}
+
+func TestCheckMeeplesAndScore(t *testing.T) {
+	// create game
+	minitileSet := tilesets.OrderedMiniTileSet2()
+	deckStack := stack.NewOrdered(minitileSet.Tiles)
+	deck := deck.Deck{Stack: &deckStack, StartingTile: minitileSet.StartingTile}
+	game, err := NewFromDeck(deck, nil, 4)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	CheckMeeplesAndScore(game, t, []uint32{0, 0}, []uint8{7, 7}, 1)
+}
+
+func TestVerifyMeepleExistence(t *testing.T) {
+	// create game
+	minitileSet := tilesets.OrderedMiniTileSet2()
+	deckStack := stack.NewOrdered(minitileSet.Tiles)
+	deck := deck.Deck{Stack: &deckStack, StartingTile: minitileSet.StartingTile}
+	game, err := NewFromDeck(deck, nil, 4)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	pos := position.New(1, 0)
+	MakeTurnValidCheck(game, t, pos, MeepleParams{MeepleType: elements.NormalMeeple, FeatureSide: side.Bottom, FeatureType: feature.Road}, true, 1) // do any correct move
+
+	VerifyMeepleExistence(t, game, pos, side.Bottom, feature.Road, true, 1)           // verify that meeple truly exists
+	VerifyMeepleExistence(t, game, pos, side.BottomLeftEdge, feature.Field, false, 1) // verify that meeple truly exists
+}
