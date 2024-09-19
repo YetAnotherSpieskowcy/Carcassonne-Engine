d134cb8cf5cdde11bfd4ae1e3add5e76472f1f05
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-09-19 11:23:43 +0200

    tilesets tests

--- pkg/tilesets/tilesets_test.go
+++ pkg/tilesets/tilesets_test.go
@@ -28,6 +28,28 @@ func TestMiniTileSet(t *testing.T) {
 	}
 }
 
+func TestMiniTileSet1(t *testing.T) {
+	var set = OrderedMiniTileSet1()
+	expected := 12
+
+	actual := len(set.Tiles)
+
+	if expected != actual {
+		t.Fatalf("got %#v tiles, should be %#v", actual, expected)
+	}
+}
+
+func TestMiniTileSet2(t *testing.T) {
+	var set = OrderedMiniTileSet2()
+	expected := 12
+
+	actual := len(set.Tiles)
+
+	if expected != actual {
+		t.Fatalf("got %#v tiles, should be %#v", actual, expected)
+	}
+}
+
 func TestEveryTileOnceTileSet(t *testing.T) {
 	var set = EveryTileOnceTileSet()
 	expected := 24
